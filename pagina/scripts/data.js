8d9407abe97d6024e059cfd79184e300396d7c49
Lucas <l.donado1@uniandes.edu.co>
2026-04-13 14:24:03 -0500

    un cambio en el js (cambio de una nombre dde imagen mal puesto)

--- pagina/scripts/data.js
+++ pagina/scripts/data.js
@@ -1,7 +1,7 @@
 import { Serie } from "./Serie.js";
 export const series = [
     new Serie(1, "Breaking Bad", "AMC", 5, "Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer.", "https://www.amc.com/shows/breaking-bad", "./images/breakingbad.jpg"),
-    new Serie(2, "Orange Is the New Black", "Netflix", 6, "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary.", "https://www.netflix.com/co/title/70242311", "./images/breakingbad.jpg"),
+    new Serie(2, "Orange Is the New Black", "Netflix", 6, "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary.", "https://www.netflix.com/co/title/70242311", "./images/orange.jpg"),
     new Serie(3, "Game of Thrones", "HBO", 7, "American fantasy drama television series created by David Benioff and D. B. Weiss.", "https://www.hbo.com/game-of-thrones", "./images/got.jpg"),
     new Serie(4, "The Big Bang Theory", "CBS", 12, "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else.", "https://www.cbs.com/shows/big_bang_theory/about/", "./images/bigbang.jpg"),
     new Serie(5, "Sherlock", "BBC", 4, "Sherlock depicts consulting detective Sherlock Holmes solving various mysteries in modern-day London.", "https://www.bbc.co.uk/programmes/b018ttws", "./images/sherlock.jpg"),
